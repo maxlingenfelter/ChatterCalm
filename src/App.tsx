a4f2a2a189fabb791b7e02fa4248225fce7a3b68
Pekay <pekaytab@gmail.com>
2023-05-25 16:31:45 -0400

    feat(Chat): :sparkles: Working chat GPT intergration

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import 'react-native-url-polyfill/auto';
 import NetInfo from "@react-native-community/netinfo";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
--- src/screens/chat.tsx
+++ src/screens/chat.tsx
@@ -1,61 +1,118 @@
-import React, { useState, useCallback } from 'react';
-import { View, Text, SafeAreaView, StyleSheet, ScrollView, Pressable, TextInput } from 'react-native';
+import React, { useState, useCallback, useEffect } from 'react';
+import {
+    View,
+    Text,
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    Pressable,
+    TextInput,
+    ActivityIndicator,
+    KeyboardAvoidingView,
+    Platform,
+    Keyboard,
+} from 'react-native';
 import moment from 'moment';
 import * as SplashScreen from 'expo-splash-screen';
-import { useFonts, Poppins_500Medium } from '@expo-google-fonts/poppins';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Configuration, OpenAIApi } from 'openai';
 import { OPENAI_API_KEY } from '.env';
 
 // Svgs
 import SendSVG from '../../assets/svgs/send.svg';
-import { useTheme } from '@react-navigation/native';
 
-const token = OPENAI_API_KEY;
 const configuration = new Configuration({
-    apiKey: token,
+    apiKey: OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-// Type for chat message
 type ChatMessageType = {
-    type: 'user' | 'bot';
-    text: string;
+    role: 'system' | 'user' | 'assistant';
+    content: string;
 };
 
-// Array of chat messages
-let MESSAGES: ChatMessageType[] = [];
-
-// Component
 const ChatScreen = ({ route }) => {
-    // Load fonts using splash-screen and Font.loadAsync
-
-
-
-    // Dynamic value for time
     const date = moment().format('LT');
-
-    // Input Value
     const [inputValue, setInputValue] = useState('');
+    const [messages, setMessages] = useState<ChatMessageType[]>([]);
+    const [isWaitingForResponse, setIsWaitingForResponse] = useState(false);
+    const [keyboardOffset, setKeyboardOffset] = useState(0);
 
-    // Event handler for button press
-    const handlePress = () => {
-        newUserMessage(inputValue);
+    const handlePress = async () => {
+        if (!inputValue) {
+            return;
+        }
+
+        const userMessage: ChatMessageType = { role: 'user', content: inputValue };
+        setMessages((prevMessages) => [...prevMessages, userMessage]);
         setInputValue('');
+        setIsWaitingForResponse(true);
+
+        try {
+            const response = await openai.createChatCompletion({
+                model: 'gpt-3.5-turbo',
+                messages: [...messages, userMessage],
+            });
+
+            console.log('OpenAI API Response:', response);
+
+            if (response && response.data && response.data.choices && response.data.choices.length > 0) {
+                const aiResponse = response.data.choices[0].message.content;
+                const botMessage: ChatMessageType = { role: 'assistant', content: aiResponse };
+                setMessages((prevMessages) => [...prevMessages, botMessage]);
+            }
+        } catch (error) {
+            console.log('OpenAI API Error:', error);
+        } finally {
+            setIsWaitingForResponse(false);
+        }
     };
 
+    useEffect(() => {
+        SplashScreen.preventAutoHideAsync();
+        SplashScreen.hideAsync();
+
+        // Set up keyboard listeners
+        const keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', (event) => {
+            if (Platform.OS === 'ios') {
+                setKeyboardOffset(event.endCoordinates.height);
+            }
+        });
+
+        const keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', () => {
+            setKeyboardOffset(0);
+        });
+
+        // Clean up listeners
+        return () => {
+            keyboardDidShowListener.remove();
+            keyboardDidHideListener.remove();
+    };
+    }, []);
+
     return (
-        <View style={styles.container}>
+        <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            style={styles.container}
+            keyboardVerticalOffset={keyboardOffset}
+        >
             <View style={styles.headerContainer}>
                 <Text style={styles.headerText}>{date}</Text>
             </View>
             <SafeAreaView style={styles.chatContainer}>
                 <ScrollView style={styles.chat}>
-                    {MESSAGES.map((message, index) => (
+                    {messages.map((message, index) => (
                         <View key={index}>
-                            <View>
-                                <Text style={styles.messageText}>{message.text}</Text>
-                            </View>
+                            {/* Distingush between messages wuth type "user" vs ones with type "assistant\" */}
+                            {message.role === 'user' ? (
+                                <View style={styles.userMessageContainer}>
+                                    <Text style={styles.userMessage}>{message.content}</Text>
+                                </View>
+                            ) : (
+                                <View style={styles.assistantMessageContainer}>
+                                    <Text style={styles.assistantMessage}>{message.content}</Text>
+                                </View>
+                            )}
                         </View>
                     ))}
                 </ScrollView>
@@ -69,6 +126,7 @@ const ChatScreen = ({ route }) => {
                                 underlineColorAndroid="transparent"
                                 value={inputValue}
                                 onChangeText={setInputValue}
+                                onFocus={() => setKeyboardOffset(0)}
                             />
                         </View>
                         <View style={styles.sendButtonContainer}>
@@ -79,18 +137,21 @@ const ChatScreen = ({ route }) => {
                                     end={{ x: 1, y: 0 }}
                                     style={styles.sendButton}
                                 >
-                                    <SendSVG />
+                                    {isWaitingForResponse ? (
+                                        <ActivityIndicator size="small" color="#fff" />
+                                    ) : (
+                                            <SendSVG />
+                                    )}
                                 </LinearGradient>
                             </Pressable>
                         </View>
                     </View>
                 </View>
             </SafeAreaView>
-        </View>
+        </KeyboardAvoidingView>
     );
 };
 
-// Styles
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -103,7 +164,6 @@ const styles = StyleSheet.create({
     },
     headerText: {
         color: '#fff',
-        fontFamily: 'Poppins_500Medium',
         fontWeight: '500',
         fontSize: 38,
         paddingHorizontal: 20,
@@ -112,7 +172,7 @@ const styles = StyleSheet.create({
     },
     chatContainer: {
         backgroundColor: '#52925b',
-        height: 660,
+        flex: 1,
     },
     chat: {},
     footerBarContainer: {
@@ -121,7 +181,6 @@ const styles = StyleSheet.create({
         paddingTop: 20,
         paddingBottom: 45,
         borderRadius: 32,
-        top: 60,
     },
     inputRow: {
         flexDirection: 'row',
@@ -131,7 +190,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     inputContainer: {
-        width: 274,
+        flex: 1,
     },
     sendButtonContainer: {
         width: 48,
@@ -150,50 +209,15 @@ const styles = StyleSheet.create({
         height: 48,
         width: 48,
         alignItems: 'center',
-        padding: 16,
+        justifyContent: 'center',
         borderRadius: 32,
     },
     messageText: {
         color: 'white',
     },
+
+    //Message Styles
+    
 });
-
-// Function to handle user message and generate AI response
-async function newUserMessage(text) {
-    const userMessage: ChatMessageType = { type: 'user', text };
-    MESSAGES.push(userMessage);
-
-    const prompt = buildPrompt(MESSAGES);
-
-    console.log('Prompt:', prompt);
-
-    try {
-        const response = await openai.createCompletion({
-            model: 'text-davinci-003',
-            prompt,
-            max_tokens: 100,
-        });
-
-        console.log('Response:', response.data);
-
-        const aiResponse = response.data.choices[0].text.trim();
-
-        const botMessage: ChatMessageType = { type: 'bot', text: aiResponse };
-        MESSAGES.push(botMessage);
-
-        console.log('AI:', aiResponse);
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
-
-// Helper function to build the prompt from the messages array
-function buildPrompt(messages) {
-    let prompt = '';
-    for (const message of messages) {
-        prompt += `${message.type === 'user' ? 'User' : 'Bot'}: ${message.text}\n`;
-    }
-    return prompt;
-}
 
 export default ChatScreen;
